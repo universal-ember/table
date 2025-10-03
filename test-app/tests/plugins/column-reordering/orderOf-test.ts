8c36696b81e92ac71f790f03f45da8c51ea4404c
johanrd <johanroed@gmail.com>
2025-10-03 14:09:49 +0200

    lint

--- test-app/tests/plugins/column-reordering/orderOf-test.ts
+++ test-app/tests/plugins/column-reordering/orderOf-test.ts
@@ -81,8 +81,19 @@ module('Plugin | column-reordering | orderOf', function () {
       ]),
     );
 
-    assert.strictEqual(result.size, 2, 'preserves all columns including hidden ones');
-    assert.deepEqual([...result.entries()], [['A', 0], ['B', 1]], 'column B was preserved (might be hidden)');
+    assert.strictEqual(
+      result.size,
+      2,
+      'preserves all columns including hidden ones',
+    );
+    assert.deepEqual(
+      [...result.entries()],
+      [
+        ['A', 0],
+        ['B', 1],
+      ],
+      'column B was preserved (might be hidden)',
+    );
   });
 
   test('handles missing columns in map (adds them)', function (assert) {
